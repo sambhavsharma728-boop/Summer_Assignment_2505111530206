a53bda46cb3c36ec631d84edcaafcf543f4fe68b
sambhavsharma728-boop <sambhavsharma728@gmail.com>
2026-07-07 19:47:37 +0530

    Day-17

---
+++ Day-17/que67.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+    int arr1[100], arr2[100];
+    int n1, n2, i, j, found;
+
+    printf("Enter the size of first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter the elements of first array:\n");
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter the size of second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter the elements of second array:\n");
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+
+    printf("Intersection of the arrays:\n");
+
+    for(i = 0; i < n1; i++) {
+        found = 0;
+
+        
+        for(j = 0; j < n2; j++) {
+            if(arr1[i] == arr2[j]) {
+                found = 1;
+                break;
+            }
+        }
+
+        
+        for(j = 0; j < i; j++) {
+            if(arr1[i] == arr1[j]) {
+                found = 0;
+                break;
+            }
+        }
+
+        if(found)
+            printf("%d ", arr1[i]);
+    }
+
+    return 0;
+}
