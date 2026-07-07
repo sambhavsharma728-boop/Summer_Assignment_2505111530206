9c8de3d839ede6be7e14428fb0bf76256ab6bef4
sambhavsharma728-boop <sambhavsharma728@gmail.com>
2026-07-07 20:00:42 +0530

    Day-20

---
+++ Day-20/que78.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10];
+    int n, i, j, flag = 1;
+
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+        if(flag == 0)
+            break;
+    }
+
+    if(flag)
+        printf("The matrix is Symmetric.");
+    else
+        printf("The matrix is Not Symmetric.");
+
+    return 0;
+}
